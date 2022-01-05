67ef80de09c1cd963fab739619b39c8b1cecd039
Trevor Wong <trevor8568@gmail.com>
2022-01-04 22:44:26 -0500

    set up serverless infra, write function to list punishments

---
+++ handler.js
@@ -0,0 +1,59 @@
+"use strict";
+
+const list = require('./routes/list').handle;
+
+module.exports.route = async (event) => {
+  if(event.requestContext === undefined
+    || event.requestContext.http === undefined
+    || event.requestContext.http.path === undefined
+    ) return {
+      statusCode: 400,
+      body: JSON.stringify(
+        {
+          success: false,
+          message: "Missing HTTP Context",          
+        },
+        null,
+        2
+      ),
+  };
+  const path = event.requestContext.http.path.toLowerCase();
+
+  if(path === '/') return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(
+      {
+        success: true,
+        version: 1.0,
+        message: "Litebans API by Captain_Sisko", 
+        github: "https://github.com/left4craft/litebans-serverless"       
+      },
+      null,
+      2
+    ),
+  };
+
+  if (path === '/list') {
+    return list(event);
+  }
+
+  return {
+    statusCode: 404,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(
+      {
+        success: false,
+        message: "No such route",
+        path: path
+      },
+      null,
+      2
+    ),
+  };
+
+};
---
+++ secret.example.js
@@ -0,0 +1,18 @@
+exports.keys = {
+	api: {
+		key: ''
+	},
+	sql: {
+		host: '',
+		user: '',
+		pass: '',
+		db: ''
+	},
+	tables: {
+		history: 'litebans_history',
+		bans: 'litebans_bans',
+		mutes: 'litebans_mutes',
+		warnings: 'litebans_warnings',
+		kicks: 'litebans_kicks'
+	}
+}
